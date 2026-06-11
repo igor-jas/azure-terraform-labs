9815fbd2edcb0f53c13b4485b57af606697e4224
Igor Jas <igorjas.cloud@gmail.com>
2026-06-11 21:25:37 +0200

    chore: initial commit

---
+++ glowny.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rgtest001"
+  location = "CentralPoland"
+}
+
+resource "azurerm_storage_account" "cache" {
+  name                     = "storagetest001"
+  resource_group_name      = azurerm_resource_group.dev.name
+  location                 = azurerm_resource_group.dev.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
