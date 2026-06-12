49067462ab866676ec471b70f6324d836f69fe6c
Igor Jas <igorjas.cloud@gmail.com>
2026-06-12 22:56:46 +0200

    refactor: update VNet and subnet configuration

--- glowny.tf
+++ glowny.tf
@@ -16,10 +16,23 @@ resource "azurerm_resource_group" "dev" {
   location = "PolandCentral"
 }
 
-resource "azurerm_storage_account" "cache" {
-  name                     = "stigorjas001"
-  resource_group_name      = azurerm_resource_group.dev.name
-  location                 = azurerm_resource_group.dev.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-nauka"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "subnet-web"
+  resource_group_name  = azurerm_resource_group.dev.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "secure" {
+  name                 = "subnet-secure"
+  resource_group_name  = azurerm_resource_group.dev.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
