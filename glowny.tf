3a64c7f6cbbc724de0367e2dc07d1ddddbd2e41e
Igor Jas <igorjas.cloud@gmail.com>
2026-06-12 08:43:38 +0200

    chore: adjust configuration for Azure free trial

--- glowny.tf
+++ glowny.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "dev" {
 }
 
 resource "azurerm_storage_account" "cache" {
-  name                     = "storagetest001"
+  name                     = "stigorjas001"
   resource_group_name      = azurerm_resource_group.dev.name
   location                 = azurerm_resource_group.dev.location
   account_tier             = "Standard"
