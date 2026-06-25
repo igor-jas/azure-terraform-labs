4c2bf83213ff0296aa390588a67b315d7f7159d6
Igor Jas <igorjas.cloud@gmail.com>
2026-06-25 11:12:17 +0200

    feat: add Linux VM and SSH lab

---
+++ lab-02-linux-vm/main.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = "rg-linux-vm-lab"
+  location = "westeurope"
+}
+
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-linux-vm-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "vm" {
+  name                 = "subnet-linux-vm"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "ssh" {
+  name                = "nsg-linux-vm-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "vm" {
+  subnet_id                 = azurerm_subnet.vm.id
+  network_security_group_id = azurerm_network_security_group.ssh.id
+}
+
+resource "azurerm_public_ip" "vm" {
+  name                = "pip-linux-vm-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm" {
+  name                = "nic-linux-vm-lab"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-linux-lab"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  size                = "Standard_D2s_v5"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("C:/Users/igorj/.ssh/azure-linux-vm-lab.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
